f0ce5483fd10e618bd923fe4899a82d77ed21dae
Emilio Andere <andere@fred.cs.uchicago.edu>
2022-02-24 15:14:58 -0600

    Flag for openimages vs imagenet

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def parse_args():
     parser.add_argument('--lr', type=float, nargs='+', default=[0.005], help='model learning rate')
     parser.add_argument('--target', type=int, nargs='+', default=[1], help='which label to use as target')
     parser.add_argument('--epochs', type=int, default=100, help='how many epochs to train for')
-
+    parser.add_argument('--data', type=str, default='OpenImages', help='OpenImages/ImageNet')
     parser.set_defaults(load_existing_triggers=False)
     return parser.parse_args()
 
@@ -44,9 +44,14 @@ def main(args):
 
     # CHANGE DATASET MANAGER AS NEEDED
     curr_path = os.getcwd()
-    #data = OpenImagesBBoxManager(dataset_root='/bigstor/rbhattacharjee1/open_images/data_old', data_root= curr_path + '/data/oi_bbox', download_data=False)
-    #data = OpenImagesManager(dataset_root='/bigstor/rbhattacharjee1/open_images/data', data_root='/home/rbhattacharjee1/phys_backdoors_in_datasets/data/oi', download_data=False)
-    data = ImageNetManager(dataset_root='/bigstor/rbhattacharjee1/ilsvrc_blurred/train', data_root= curr_path + '/data/imagenet', download_data=False)
+    
+    # Logical condition for either OpenImages or ImageNet path
+    if (args.data == "OpenImages"):
+        data = OpenImagesBBoxManager(dataset_root='/bigstor/rbhattacharjee1/open_images/data_old', data_root= curr_path + '/data/oi_bbox', download_data=False)
+        # data = OpenImagesManager(dataset_root='/bigstor/rbhattacharjee1/open_images/data', data_root='/home/rbhattacharjee1/phys_backdoors_in_datasets/data/oi', download_data=False)
+    elif (args.data == "ImageNet"):
+        data = ImageNetManager(dataset_root='/bigstor/rbhattacharjee1/ilsvrc_blurred/train', data_root= curr_path + '/data/imagenet', download_data=False)
+
     num_clean = args.sample_size
     num_poison = int(args.sample_size * args.inject_rate) + 3 #WHYYYY
 
